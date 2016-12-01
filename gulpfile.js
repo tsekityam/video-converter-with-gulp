c140986153642e41ae21fccb15a2a903fe201e7e
Tse Kit Yam <me@kytse.com>
2016-12-01 20:04:37 +0800

    Revert "Change to use suggested option if output to mp4"
    This reverts commit 1214f9922ff9e6e0758afadbbf0943a795d71613.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task('mkv_to_mp4', function () {
   return gulp.src('src/*/*.mkv')
   .pipe(ffmpeg('mp4', function (cmd) {
     return cmd
-    .outputOptions('-movflags frag_keyframe+faststart');
+    .outputOptions('-movflags frag_keyframe+empty_moov');
   }))
   .pipe(gulp.dest('dest/'));
 });
