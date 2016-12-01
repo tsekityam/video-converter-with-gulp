a3deed004cfded531d12329f37802e3a503f0e4b
Tse Kit Yam <me@kytse.com>
2016-12-01 21:55:16 +0800

    Fix: not all file in the path will be transcoded

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var ffmpeg = require('gulp-fluent-ffmpeg');
 
 gulp.task('mkv_to_mp4', function () {
-  return gulp.src('src/*/*.mkv')
+  return gulp.src('src/**/*.mkv')
   .pipe(ffmpeg('mp4', function (cmd) {
     return cmd
     .outputOptions('-movflags frag_keyframe+empty_moov');
